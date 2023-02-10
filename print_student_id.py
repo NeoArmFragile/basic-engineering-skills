687dfc22eb9eeb9c3914ed79d27ba8f644d24420
Good Morning <124991842+NeoArmFragile@users.noreply.github.com>
2023-02-10 14:58:04 +0700

    Second Banana

--- print_student_id.py
+++ print_student_id.py
@@ -1,5 +1,11 @@
+import numpy as np
+
 def print_id(student_id):
     print("student ID = ", student_id)
 
+def calculate_sum(array):
+    return array.sum()
+
 if __name__ == "__main__":
-    print_id("6513385") 
+    print_id("6513385")
+    calculate_sum(np.array([1,2,3])) 
